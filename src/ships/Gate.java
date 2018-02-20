d90d639e94be4844db8e7e3797e6bd7ca84f8dd2
Jose Maria Crespo <jocrespos@alumnos.unex.es>
2018-02-20 23:45:01 +0100

    Añadido Singleton a Gate

--- src/ships/Gate.java
+++ src/ships/Gate.java
@@ -2,6 +2,22 @@ package ships;
 
 public class Gate {
 
+	//private int id;
+	
+	private static Gate instance;
+	
+	private void Gate() {
+	}
+//	private void Gate(int _id) {
+//		id = _id;
+//	}
+//	
+	public Gate getGate() {
+		if(instance == null)
+			instance = new Gate();
+		return instance;
+	}
+	
 	public static synchronized void enter(Ship s) {
 		for(int i=0;i<3;i++)
 			System.out.println("The ship " + s.name + " is entering.");
