f16286d6a17af0ff4cd68fcbafd2c5fd8a2ee776
Jose Maria Crespo <jocrespos@alumnos.unex.es>
2018-02-27 12:27:08 +0100

    Probemos

--- src/ships/Control.java
+++ src/ships/Control.java
@@ -5,6 +5,8 @@ public class Control {
 	private int entering;
 	private int exiting;
 	
+	private int waitExit;
+	
 	private static Control instance;
 	
 	private Control() {
@@ -20,8 +22,10 @@ public class Control {
 
 	//Entry protocol
 	public synchronized void entryPermission(Ship s) {
-		while (exiting > 0)
+		while (exiting > 0 && waitExit > 0)
 			try {
+				System.out.println(s.name+" waiting for entering...");
+
 				wait();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
@@ -39,6 +43,8 @@ public class Control {
 	public synchronized void exitPermission(Ship s) {
 		while(entering>0)
 			try {
+				System.out.println(s.name+" waiting for exiting...");
+				waitExit++;
 				wait();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
@@ -48,6 +54,7 @@ public class Control {
 	
 	public synchronized void exitNotification(Ship s) {
 		exiting--;
+		waitExit--;
 		if(exiting==0)
 			notifyAll();
 	}
