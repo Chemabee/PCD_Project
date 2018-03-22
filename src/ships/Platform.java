ce962a48c170cf1d8d37db374d9ee6b772f25b38
Jose Maria Crespo <jocrespos@alumnos.unex.es>
2018-03-22 14:15:51 +0100

    POS ESO

--- src/ships/Platform.java
+++ src/ships/Platform.java
@@ -8,7 +8,7 @@ public class Platform extends Monitor {
 	public int type;	// 0 = empty // 1 = sugar // 2 = flour // 3 = salt //
 	private static Platform instance=null;
 	Lock l = new ReentrantLock();
-	
+
 	Condition salt = l.newCondition();
 	Condition sugar = l.newCondition();
 	Condition flour = l.newCondition();
@@ -27,42 +27,44 @@ public class Platform extends Monitor {
 	void getProduct(int craneType) {
 		while(true) {
 			l.lock();
-			while(type != craneType){
-				try{
-					switch(craneType){
-						case 1:
-							sugar.await();
-							type = 0; //Remove the container from the platform
-							System.out.println("*Crane 1 removed a sugar container from the platform.");
-							cargo.signal();
-							break;
-						case 2:
-							flour.await();
-							type = 0; //Remove the container from the platform
-							System.out.println("#Crane 2 removed a flour container from the platform.");
-							cargo.signal();
-							break;
-						case 3:
-							salt.await();
-							type = 0; //Remove the container from the platform
-							System.out.println("$Crane 3 removed a salt container from the platform.");
-							cargo.signal();
-							break;
-						}
-					}
-					catch  (InterruptedException e){
-						e.printStackTrace();
-					}	finally{
-						l.unlock();}
+			try {
+				switch (craneType) {
+				case 1:
+					while (type != craneType)
+						sugar.await();
+					type = 0; // Remove the container from the platform
+					System.out.println("*Crane 1 removed a sugar container from the platform.");
+					cargo.signal();
+					break;
+				case 2:
+					while (type != craneType)
+						flour.await();
+					type = 0; // Remove the container from the platform
+					System.out.println("#Crane 2 removed a flour container from the platform.");
+					cargo.signal();
+					break;
+				case 3:
+					while (type != craneType)
+						salt.await();
+					type = 0; // Remove the container from the platform
+					System.out.println("$Crane 3 removed a salt container from the platform.");
+					cargo.signal();
+					break;
 				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} finally {
+				l.unlock();
+			}
 		}
 	}
 
 	void depositProduct(int containerType){
 		l.lock();
-		while(type != 0){
-			try {
+		try {
+			while(type != 0){
 				cargo.await();
+				}
 				type = containerType; //Deposit the item in the Platform
 				System.out.println("Cargo ship has deposited a container "+type+" in the platform.");
 			} catch (InterruptedException e) {
@@ -70,7 +72,6 @@ public class Platform extends Monitor {
 			} finally{
 				l.unlock();
 			}
-		}
 	}
 
 
